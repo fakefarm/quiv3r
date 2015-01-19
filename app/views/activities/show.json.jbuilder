c5a8d6c4d6475b00f866ad9d210d4b953b1e39c5
Dave Woodall <dave.woodall@livingsocial.com>
2015-01-19 13:49:34 -0700

    mvp kickoff

---
+++ app/views/activities/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @activity, :id, :title, :body, :public, :website, :location, :price, :age, :hours, :environment, :season, :created_at, :updated_at
---
+++ db/migrate/20150119194023_create_activities.rb
@@ -0,0 +1,29 @@
+class CreateActivities < ActiveRecord::Migration
+  def change
+    create_table :activities do |t|
+      t.string :title
+      t.string :body
+      t.string :category #food, playground outdoor, playground outdoor, climbing, trail, skiing, hiking, biking, water, camping, adventure misc., bible, educational, creative, crafts, technology, kids-eat-free-night
+      # kids eat free night
+      # in door playground
+      # swimming pool / lake
+      # fishing area
+      # kid friendly biking
+      # skiing
+      t.boolean :public
+      t.string :website
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zip
+      t.string :price
+      t.string :age
+      t.string :hours
+      t.string :environment
+      t.string :season
+      t.string :submitter
+
+      t.timestamps
+    end
+  end
+end
