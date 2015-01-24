25fc5026b74f70e42205ed14138df4757f4d9596
Dave Woodall <dave.woodall@livingsocial.com>
2015-01-24 13:00:01 -0700

    Filter by tags

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :activities
   root 'activities#index'
 
+  get 'tag/:category' => 'activities#tag'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
