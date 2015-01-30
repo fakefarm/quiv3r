eb2ed95f2e314079b0a77ac24030574d1a7d6848
Dave Woodall <dave.woodall@livingsocial.com>
2015-01-30 06:37:25 -0700

    move to postgres

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ gem 'bourbon'
 gem 'neat'
 gem 'bitters'
 gem 'refills'
-gem 'sqlite3', group: :development
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
@@ -18,6 +17,6 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 
 gem 'spring',        group: :development
 
-gem 'pg', group: :production
+gem 'pg'
 gem 'rails_12factor', group: :production
 gem 'pry', group: :development
--- db/migrate/20150119194023_create_activities.rb
+++ db/migrate/20150119194023_create_activities.rb
@@ -1,28 +1,11 @@
 class CreateActivities < ActiveRecord::Migration
   def change
     create_table :activities do |t|
-      t.string :title
-      t.string :body
+      t.string  :title
+      t.text    :body
       t.boolean :public
-      t.string :author
-      t.string :website
-      t.string :google_url
-      t.string :category #food, playground outdoor, playground outdoor, climbing, trail, skiing, hiking, biking, water, camping, adventure misc., bible, educational, creative, crafts, technology, kids-eat-free-night
-      # kids eat free night
-      # in door playground
-      # swimming pool / lake
-      # fishing area
-      # kid friendly biking
-      # skiing
-      # t.string :address
-      # t.string :city
-      # t.string :state
-      # t.integer :zip
-      # t.string :price
-      # t.string :age
-      # t.string :hours
-      # t.string :environment
-      # t.string :season
+      t.string  :author
+      t.string  :tag
 
       t.timestamps
     end
